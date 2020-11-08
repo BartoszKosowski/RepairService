32bb15bd5a361fdecc92f23312a15a4a59cc7680
oktawian 1750 <piotrektalik1@gmail.com>
2020-11-08 18:41:10 +0100

    Witam

---
+++ Proejkt_BD/Control/Menager/Menager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proejkt_BD.Control.Menager
+{
+    public partial class Menager : UserControl
+    {
+        public Menager()
+        {
+            InitializeComponent();
+        }
+    }
+}
